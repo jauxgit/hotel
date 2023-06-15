9ac206afe7469a7082090a28e9fd73a32972a9f7
16778 <16778818803@qq.com>
2023-06-15 10:45:42 +0800

    实训第二天

--- src/main/java/com/jaux/hotel/pojo/Student.java
+++ src/main/java/com/jaux/hotel/pojo/Student.java
@@ -2,9 +2,11 @@ package com.jaux.hotel.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private int id;
     private String name;
